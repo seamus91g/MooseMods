88d6ae027182f947c39580d0c144082d8f050b68
Seamus G <seamus.g91@gmail.com>
2018-11-15 21:35:03 +0000

    Adding all website files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Moose Mods Website
+Created in 2009 by John M and Seamus G 
+## Link
+The website is currently hosted on a Raspberry Pi, and can be viewed here: http://moosemods.xyz 
---
+++ aboutus.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+
+
+<html>
+<head>
+<title>Home page – My Website</title>
+
+<link rel="shortcut icon" type="image/x-icon" href="Images/favicon.ico">
+
+<link rel="stylesheet" type="text/css" href="header.css" />
+
+</head>
+
+<body background="Images/BodyBg.jpg">
+
+<?php include("header.html");?>
+
+<?php include("aboutus.html");?>
+
+<?php include("footer.html");?>
+
+</body>
+</html>
+
